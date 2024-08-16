c982808fb9e92763d6d3ebbb33c259d995ba397a
Prabhu Prasad Sahoo <prabhupps32@gmail.com>
2024-08-16 22:42:58 +0530

    Update project.py

--- project.py
+++ project.py
@@ -30,7 +30,7 @@ def improved_recommendations(title,movies_preprocessed):
     idx = indices[title]
     sim_scores = list(enumerate(cosine_sim[idx]))
     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
-    sim_scores = sim_scores[1:21]
+    sim_scores = sim_scores[1:30]
     movie_indices = [i[0] for i in sim_scores]
 
     pred = movies_preprocessed.iloc[movie_indices][['title', 'vote_count', 'vote_average', 'year']]
